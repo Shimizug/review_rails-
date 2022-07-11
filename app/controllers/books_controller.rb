a1b90d1302aa79e1c4ec3b1fc6d59af206c876c6
Shimizug <106298106+Shimizug@users.noreply.github.com>
2022-07-11 03:14:07 +0000

    [Add]閲覧数の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,13 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    unless ViewCount.find_by(user_id: current_user.id, book_id: @book.id)
+      current_user.view_counts.create(book_id: @book.id)
+    end
     @book_new = Book.new
     @book_comment = BookComment.new
   end
-
+  
   def index
     to  = Time.current.at_end_of_day
     from  = (to - 6.day).at_beginning_of_day
